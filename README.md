ca8b94242eae769d7c31eb00db67260e7a774067
Haoyu Liang <hyliang96@163.com>
2018-08-31 17:23:34 +0800

    logout push

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # sharedotfile
 the shared dotfiles among all my servers, mac and cp (WSL)
 
+
 ### install
 * git clone this repo to a new computer, put the dir on any path you like
 
--- pushdotfile.sh
+++ pushdotfile.sh
@@ -2,9 +2,10 @@
 
 cd ~/.sharedotfile
 
-
-	if ! [ -z "$(git status --porcelain)" ]; then
-		git add -A &> ~/.sharedotfile_log/push.log
-		git commit -m "logout push" &>> ~/.sharedotfile_log/push.log
-		git push origin master &>> ~/.sharedotfile_log/push.log
-	fi
+{
+if ! [ -z "$(git status --porcelain)" ]; then
+	git add -A && \
+	git commit -m "logout push" --quiet && \
+	git push origin master --quiet; 
+fi;
+}&
