61da70663521161a6079fc8a61f0befd5868ee1a
Haoyu Liang <hyliang96@163.com>
2018-08-31 17:14:29 +0800

    logout push

--- pushdotfile.sh
+++ pushdotfile.sh
@@ -2,10 +2,9 @@
 
 cd ~/.sharedotfile
 
-{
+
 	if ! [ -z "$(git status --porcelain)" ]; then
-		git add -A    && \
-		git commit -m "logout push"   && \
-		git push origin master 
-	fi;
-} &>> .log/push.log
+		git add -A &> ~/.sharedotfile_log/push.log
+		git commit -m "logout push" &>> ~/.sharedotfile_log/push.log
+		git push origin master &>> ~/.sharedotfile_log/push.log
+	fi
