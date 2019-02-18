9b300382718a86d35f882aa0b23e2c4b04644932
Haoyu Liang <hyliang96@163.com>
2019-02-18 20:41:38 +0800

    logout push

--- hidden_cmd.py
+++ hidden_cmd.py
@@ -4,8 +4,10 @@ run a line of command and get its return without outputing on displayer
 import subprocess
 
 def hidden_command(command, code = 'utf-8'):
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, 
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE, close_fds=True)
     output_byte_line_list = p.stdout.readlines()
     output = ''.join([line.decode(code) for line in output_byte_line_list])
+    if len(output)>0 and output[-1]=='\n':
+        output = output[:-1]
     return output
